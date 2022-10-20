9d457cdd60bdfc2bfcf88e8cbd16272844bca5db
hammyhammer <hammyjacobs@gmail.com>
2022-10-20 16:19:19 -0400

    completed problem two

--- Basics/Objects/objectsExercise.ts
+++ Basics/Objects/objectsExercise.ts
@@ -41,3 +41,7 @@ const cats: Movie = {
 
 // For example...
 // getProfit(cats) => -21166652
+
+function getProfit(movie: Movie): number {
+  return movie.boxOffice.grossWorldwide - movie.boxOffice.budget
+}
