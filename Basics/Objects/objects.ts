9a8e81889fffac7352040d6f66f57672ea50d915
hammyhammer <hammyjacobs@gmail.com>
2022-10-20 14:52:39 -0400

    excess properties of TS objects

--- Basics/Objects/objects.ts
+++ Basics/Objects/objects.ts
@@ -14,3 +14,11 @@ let coordinate: { x: number, y: number } = { x: 34, y: 2 };
 function randomCoordinate(): { x: number, y: number } {
   return { x: Math.random(), y: Math.random() }
 }
+
+// The Example below will throw an error because on line 3, we do not declare age in the initially.
+printName({ first: "Potato", last: "head", age: 40 })
+
+// However if make a new variable, even with keys that printName does not have, it does not throw errors.
+// As long we have the properties that are defined, TS doesnt care about excess properties 
+let toy = { first: 'potato', last: 'head', age: 40 }
+printName(toy)
