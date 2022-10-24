0135781ca16eeeb6d195be6b0a77b6e381dc3f49
hammyhammer <hammyjacobs@gmail.com>
2022-10-24 10:36:34 -0400

    basic syntax for arrays

---
+++ Basics/Arrays/arrays.ts
@@ -0,0 +1,13 @@
+// We can annotate arrays like we do for variables.
+const users: string[] = ["bob"]
+
+const age: number[] = []
+age.push(1)
+age.push(192)
+
+const boolean: boolean[] = [true]
+
+// We can do an any type array
+const any = []
+
+
