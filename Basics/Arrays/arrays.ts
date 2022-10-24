599d3612b2dd201be80c585ee083043991bf6ed4
hammyhammer <hammyjacobs@gmail.com>
2022-10-24 10:51:34 -0400

    added multidimensional arrays syntax

--- Basics/Arrays/arrays.ts
+++ Basics/Arrays/arrays.ts
@@ -11,3 +11,21 @@ const boolean: boolean[] = [true]
 const any = []
 
 
+// Different syntax
+const different: Array<string> = ['test']
+
+// Custom types. We can use these in our arrays.
+type Point = {
+  x: number,
+  y: number
+}
+
+const coords: Point[] = []
+coords.push({ x: 23, y: 8 })
+
+//Multi dimensional. A single [] wouldn't work, we have to say that there are [][]
+const board: string[][] = [
+  ["X", "O", "X"],
+  ["X", "O", "X"],
+  ["X", "O", "X"]
+]
