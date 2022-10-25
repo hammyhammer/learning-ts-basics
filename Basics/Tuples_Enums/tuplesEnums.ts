1d1dddd9f0b42e09f172ce8415b31fda24b827dd
hammyhammer <hammyjacobs@gmail.com>
2022-10-25 10:25:41 -0400

    quick example of a tuple

--- Basics/Tuples_Enums/tuplesEnums.ts
+++ Basics/Tuples_Enums/tuplesEnums.ts
@@ -1,3 +1,6 @@
 // Tuples are unique in TS. They do not exist in JS.
 // Array with a fixed length and ordered with specific types.
 
+// We create a variable and then specifically say how we want our array to be ordered.
+// if we add a string or another number (to increase elements to 4) we will get yelled at. 
+const color: [number, number, number] = [255, 0, 255]
