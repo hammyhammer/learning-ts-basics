a0717ba7b571dc8578f0cb2745ab62fb4240def2
hammyhammer <hammyjacobs@gmail.com>
2022-10-26 14:40:26 -0400

    tried out tsc -w

--- Basics/Compiler/index.ts
+++ Basics/Compiler/index.ts
@@ -1,11 +1,16 @@
+// tsc -w index.ts allows the terminal to display any errors and provide realtime errors
+// so we do not have to tsc index.ts all the time.
+
 interface Chicken {
   breed: string,
   eggsPerWeek: Number,
-  name: string
+  name: string,
+  age: number
 }
 
 const norma: Chicken = {
   breed: "Silkie",
   eggsPerWeek: 4,
-  name: "Norma"
+  name: "Norma",
+  age: 2
 }
