2ba348342dee3de9972f7bac3980c16c0943bfce
hammyhammer <hammyjacobs@gmail.com>
2022-10-26 15:03:18 -0400

    include and exclude examples

--- Basics/Compiler/dontCompile.ts
+++
@@ -1,2 +0,0 @@
-// Going to the tsconfig.json; at the bottom, we can decalre what files we want to compile.
-// If it is absent, it will assume to do all, but we can specify which files we want to compile. 
---
+++ Basics/Compiler/src/dontCompile.ts
@@ -0,0 +1,4 @@
+// Going to the tsconfig.json; at the bottom, we can decalre what files we want to compile.
+// If it is absent, it will assume to do all, but we can specify which files we want to compile. 
+
+// Include will compile whatever is in the array, while exclude will not compile them
