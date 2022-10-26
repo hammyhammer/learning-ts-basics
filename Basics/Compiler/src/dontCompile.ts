c4d4c04aa452205c399d2d79f8d6b597999204b4
hammyhammer <hammyjacobs@gmail.com>
2022-10-26 15:19:04 -0400

    outDir option

--- Basics/Compiler/src/dontCompile.ts
+++ Basics/Compiler/src/dontCompile.ts
@@ -1,4 +1,10 @@
 // Going to the tsconfig.json; at the bottom, we can decalre what files we want to compile.
 // If it is absent, it will assume to do all, but we can specify which files we want to compile. 
 
-// Include will compile whatever is in the array, while exclude will not compile them
+// Include will compile whatever is in the array, while exclude will not compile them.
+
+// You would put a node modules in the exclude to not compile dependencies 
+
+
+// We can organize our JS files into dist. But we need to go into the json to select the folder we want to put it in
+// outDir is what will we look for  
