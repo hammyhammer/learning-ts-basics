90746accd17362ecebee49b1e751a7e07ad4924c
hammyhammer <hammyjacobs@gmail.com>
2022-10-27 13:02:07 -0400

    type assertion example

--- Basics/MiniProject/dist/index.js
+++ Basics/MiniProject/dist/index.js
@@ -1,7 +1,14 @@
 "use strict";
+// The ! at the end is telling TS to stop worrying, it is not null. 
 const btn = document.getElementById("btn");
 // Without the ?, TS will yell at us saying the btn ccould be null. We get some control
 // by saying its a maybe. 
 btn === null || btn === void 0 ? void 0 : btn.addEventListener("click", function () {
     alert("Clicked");
 });
+// Type assertion.
+// If for whatever reason we want to force a type onto a variable we can do the following:
+// Unknown is a type, it wont think itself as a string, number, etc
+// let mystery: unknown = "Hello World"
+// We can add mystery as string to force it to be a string. Without it, it will yell at us about the length
+// const numChars = (mystery as string).length
