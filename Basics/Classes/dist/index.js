a50636c6a787429c8c81ab34cfb1c055f56848bf
hammyhammer <hammyjacobs@gmail.com>
2022-10-31 14:42:52 -0400

    getters

--- Basics/Classes/dist/index.js
+++ Basics/Classes/dist/index.js
@@ -1,8 +1,9 @@
+
 class Player {
   // This is a special case where JS will know that every instance of Player will
   // have this properties. We do not have to explicity call for it.
   #score = 0; // Only usable in the player class
-  numLives = 10;
+  #numLives = 10; // Private field
   constructor(first, last) {
     this.first = first;
     this.last = last;
@@ -11,9 +12,16 @@ class Player {
     console.log("Constructing")
   }
 
-  getScore() {
-    return this.#score
+  // Using the get key word allows us to make a method to pull 
+  // certain information from our class
+  get fullName() {
+    return `${this.first} ${this.last}`
   }
+
+  // get score() {
+  //   return this.#score
+  // }
+
 
   taunt() {
     console.log("Get rekt")
@@ -28,3 +36,6 @@ const player1 = new Player("Snoopy", "dawg")
 player1.taunt()
 console.log(player1.first)
 console.log(player1.last)
+
+console.log(player1.fullName)
+// console.log(player1.score)
