677125d34fc21a976886f917ec4a06266d6adbe9
hammyhammer <hammyjacobs@gmail.com>
2022-10-31 13:08:44 -0400

    constructors

--- Basics/Classes/dist/index.js
+++ Basics/Classes/dist/index.js
@@ -1,8 +1,15 @@
 class Player {
+  constructor(first, last) {
+    this.first = first;
+    this.last = last;
+    console.log("Constructing")
+  }
   taunt() {
     console.log("Get rekt")
   }
 }
 
-const player1 = new Player()
+const player1 = new Player("Snoopy", "dawg")
 player1.taunt()
+console.log(player1.first)
+console.log(player1.last)
