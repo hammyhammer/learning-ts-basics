eaf6dd61e4e6e1e14087ee17e40d66d8c3f48565
hammyhammer <hammyjacobs@gmail.com>
2022-11-01 11:12:35 -0400

    copied and pasted class

--- Basics/Classes/dist/index.js
+++ Basics/Classes/dist/index.js
@@ -2,22 +2,39 @@
 // We still need to annonate the type 
 // A primary difference between JS classes and TS classes is that we need to declare
 // the type pror to the constructor. If we do not it will yell at us. 
+// class Player {
+//   // Readonly allows us to never change it again. Once it is set, you cannot modify it.
+//   // By default, all methods and classes are public unless otherwise. We can be
+//   // explicit about it by putting public keyword in. Public allows anyone to access
+//   // whatever information inside or outside of the class
+//   public readonly first: string;
+//   public readonly last: string;
+//   // We do not have to be specific if we assign something to the variable. Score in this
+//   // case is hardcoded in. All players will have the same starting score, but all players
+//   // will have different names, hence why we do not assign anything to them.
+//   // We could leave off the : number but it is good practice to be specific about it
+//   // With the private keyword, it cannot be accessed outside of the class. Similar to 
+//   // the JS #score. It can be used in TS but either will work. 
+//   private score: number = 0;
+//   constructor(first: string, last: string) {
+//     this.first = first;
+//     this.last = last;
+//     // Example of we can use private methods within the class but cannot access
+//     // them outside of the class
+//     this.privateMethod()
+//   }
+//   // Example of a private method
+//   private privateMethod(): void {
+//     console.log("Secreeeeet")
+//   }
+// }
 class Player {
     constructor(first, last) {
-        // We do not have to be specific if we assign something to the variable. Score in this
-        // case is hardcoded in. All players will have the same starting score, but all players
-        // will have different names, hence why we do not assign anything to them.
-        // We could leave off the : number but it is good practice to be specific about it
-        // With the private keyword, it cannot be accessed outside of the class. Similar to 
-        // the JS #score. It can be used in TS but either will work. 
         this.score = 0;
         this.first = first;
         this.last = last;
-        // Example of we can use private methods within the class but cannot access
-        // them outside of the class
         this.privateMethod();
     }
-    // Example of a private method
     privateMethod() {
         console.log("Secreeeeet");
     }
