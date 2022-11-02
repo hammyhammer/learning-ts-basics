a9f046a99fb077330372714f41d72f4fd2f92cde
hammyhammer <hammyjacobs@gmail.com>
2022-11-02 11:01:03 -0400

    praciticing generics

--- Basics/Generics/generics.ts
+++ Basics/Generics/generics.ts
@@ -6,3 +6,39 @@ const numbers: Array<number> = [];
 let inputElement = document.querySelector<HTMLInputElement>("#username")!;
 
 let button = document.querySelector<HTMLButtonElement>("btn")!;
+
+// ==============================================================
+
+// Identity is a common name to use for generics
+function numberIdentity(item: number) {
+  return item
+}
+
+function stringIdentity(item: string) {
+  return item
+}
+
+function booleanIdentity(item: boolean) {
+  return item
+}
+
+//Any types defeats the purpose of Typescript. 
+function identity(item: any): any {
+  return item
+}
+
+function indentity<Type>(item: Type): Type {
+  return item
+}
+// Above: Type can be written as just T. It could be anything but it is mostly T
+// TS will know it will be a number, even though we put Type above. We can take
+// advantage of it and customize with it.  
+indentity<number>(7)
+indentity<string>('7')
+
+interface Cat {
+  name: string;
+  breed: string;
+}
+
+indentity < Cat > {()}
