b45f94205bac3891eb1aae51e928e7cf0c04323f
hammyhammer <hammyjacobs@gmail.com>
2022-11-04 11:31:27 -0400

    instanceof

--- Basics/Narrowing/narrowing.ts
+++ Basics/Narrowing/narrowing.ts
@@ -16,3 +16,33 @@ function getDuration(media: Movie | TVShow) {
   }
   return media.duration
 }
+
+// We can use instancesof operator to check if something is an instance of another
+// Good for classes
+function printFullDate(date: string | Date) {
+  if (date instanceof Date) {
+    console.log(date.toUTCString())
+  } else {
+    console.log(new Date(date).toUTCString())
+  }
+}
+
+class User {
+  constructor(public username: string) { }
+
+}
+
+class Company {
+  constructor(public name: string) { }
+}
+
+function printName(entity: User | Company) {
+  // Is it from the User class?
+  if (entity instanceof User) {
+    entity
+  }
+  // Not from User class, therefore must be Company class
+  else {
+    entity
+  }
+}
