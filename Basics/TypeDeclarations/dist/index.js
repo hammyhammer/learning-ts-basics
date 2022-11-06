702a3efea463f69710b0965c46c42a689827f37e
hammyhammer <hammyjacobs@gmail.com>
2022-11-06 08:17:19 -0500

    installed types

--- Basics/TypeDeclarations/dist/index.js
+++ Basics/TypeDeclarations/dist/index.js
@@ -5,6 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 // We installed axois, now we just have to import it
 const axios_1 = __importDefault(require("axios"));
+// To learn what methods we can use, hover over an underscore
 // To make a basic get request
 axios_1.default.get("http://jsonplaceholder.typicode.com/users/1")
     .then((res) => {
--- Basics/TypeDeclarations/src/index.ts
+++ Basics/TypeDeclarations/src/index.ts
@@ -1,6 +1,12 @@
 // We installed axois, now we just have to import it
 import axios from "axios";
 
+// Lodash does not come in with Types, unlike axios.
+// We need to find individual types else where. We can find popular repos that would have them
+
+import _ from "lodash";
+// To learn what methods we can use, hover over an underscore
+
 // To make a basic get request
 axios.get("http://jsonplaceholder.typicode.com/users/1")
   .then((res) => {
