0507315b8f7d9c9c80975dc65524047d13034944
hammyhammer <hammyjacobs@gmail.com>
2022-11-07 13:48:59 -0500

    type importing

--- Basics/Modules/src/user.ts
+++ Basics/Modules/src/user.ts
@@ -1,4 +1,6 @@
-export default class User {
+import type { Person } from "./types.js"
+
+export default class User implements Person {
   constructor(public username: string, public email: string) { }
   logout() {
     console.log(`${this.username} logs out`)
